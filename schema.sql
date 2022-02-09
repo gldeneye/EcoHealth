f72e033531b45634d4ead1c1bf302c8fcbb8f13e
niklas sandberg <niklas.sandberg@gmail.com>
2022-02-09 09:00:27 +0100

    Second

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Product(
 Id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(64) NOT NULL,
-numberOfTokens INT NOT NULL);
+numberOfTokens INT);
 
 CREATE TABLE Agreement(
 Id INT PRIMARY KEY,
@@ -22,10 +22,12 @@ lastName VARCHAR(64) NOT NULl,
 email VARCHAR(64)
 );
 
-ALTER TABLE Agreement ADD FOREIGN KEY (product.productId) REFERENCES Product(Id)
-ALTER TABLE Agreement ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
-ALTER TABLE Token ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
-ALTER TABLE Token ADD FOREIGN KEY (signedAgreement) REFERENCES Customer(Id)
+ALTER TABLE Agreement ADD FOREIGN KEY (product.productId) REFERENCES Product(Id);
+ALTER TABLE Agreement ADD FOREIGN KEY (customerId) REFERENCES Customer(Id);
+ALTER TABLE Token ADD FOREIGN KEY (customerId) REFERENCES Customer(Id);
+ALTER TABLE Token ADD FOREIGN KEY (signedAgreement) REFERENCES Customer(Id);
+ALTER TABLE Token ADD FOREIGN KEY (signedAgreement) REFERENCES Product(Id);
+ALTER TABLE Token ADD FOREIGN KEY (customerId) REFERENCES Product(Id);
 
 
 
