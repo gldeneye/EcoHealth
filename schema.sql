81fca1432d186c64767b575bf61f784adf9f4735
niklas sandberg <niklas.sandberg@gmail.com>
2022-02-09 08:03:13 +0100

    Second

--- schema.sql
+++ schema.sql
@@ -1,30 +1,31 @@
 CREATE TABLE Product(
-Id INT AUTO_INCREMENT NOT NULL,
+Id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(64) NOT NULL,
 numberOfTokens INT NOT NULL);
 
 CREATE TABLE Agreement(
-Id INT NOT NULL,
+Id INT PRIMARY KEY,
 customerId INT NOT NULL,
-product.productId
+product.productId INT
 );
 
 CREATE TABLE Token(
-customer.id INT NOT NULL,
-signedAgreement timeStamp NOT NULL
+customerId INT PRIMARY KEY,
+productId INT NOT NULL,
+signedAgreement timeStamp PRIMARY KEY
 );
 
 CREATE TABLE Customer(
-customerId NOT NULL,
+Id INT PRIMARY KEY,
 firstName VARCHAR(64) NOT NULL,
 lastName VARCHAR(64) NOT NULl,
 email VARCHAR(64)
 );
 
-ALTER TABLE Product ADD FOREIGN KEY (product.productId) REFERENCES Product(Id)
-ALTER TABLE Customer ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
-ALTER TABLE token ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
-ALTER TABLE Product ADD FOREIGN KEY () REFERENCES Customer(Id)
+ALTER TABLE Agreement ADD FOREIGN KEY (product.productId) REFERENCES Product(Id)
+ALTER TABLE Agreement ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
+ALTER TABLE Token ADD FOREIGN KEY (customerId) REFERENCES Customer(Id)
+ALTER TABLE Token ADD FOREIGN KEY (signedAgreement) REFERENCES Customer(Id)
 
 
 
