a57eb8531d06295c376a9a486e8773f08a57ad45
Robyn Doyle <robyn.doylee@hotmail.com>
2022-02-10 10:36:22 +0100

    robyns commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,14 @@ INSERT INTO CUSTOMER(persNo, password, firstName, lastName) VALUES ('720327-5933
 INSERT INTO CUSTOMER(persNo, password, firstName, lastName)  VALUES ('720327-5934', 'password123', 'Robyn', 'Doyle');
 INSERT INTO CUSTOMER(persNo, password, firstName, lastName)  VALUES ('720327-5935', 'password123', 'Niklas', 'Sandberg');
 INSERT INTO CUSTOMER(persNo, password, firstName, lastName)  VALUES ('720327-5936', 'password123', 'Niklas', 'Sandberg');
+
+INSERT INTO Product(name, numberOfTokens) VALUES ('Mortgage', 5);
+INSERT INTO Product(name, numberOfTokens) VALUES ('ChildrensSavings', 2);
+INSERT INTO Product(name, numberOfTokens) VALUES ('PensionsSavings', 2);
+INSERT INTO Product(name, numberOfTokens) VALUES ('Insurance', 1);
+
+INSERT INTO Agreement(customerId, productId, agreementSignedDate, agreementSignedTime) VALUES (1, 1, '2022-01-01','10:00:00');
+INSERT INTO Agreement(customerId, productId, agreementSignedDate, agreementSignedTime) VALUES (2, 2, '2022-01-01','10:00:00');
+INSERT INTO Agreement(customerId, productId, agreementSignedDate, agreementSignedTime) VALUES (3, 2, '2022-01-01','10:00:00');
+
+INSERT INTO CustomerInfo(customerId, maritalStatus, children, accommodation) VALUES (1, 'Single', 0, 'Rental');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,33 @@ firstName VARCHAR(64) NOT NULL,
 lastName VARCHAR(64) NOT NULL,
 email VARCHAR(64)
 );
+
+CREATE TABLE Product(
+Id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(64) NOT NULL,
+numberOfTokens INT
+);
+
+CREATE TABLE Agreement(
+Id INT AUTO_INCREMENT PRIMARY KEY,
+customerId INT,
+productId INT,
+agreementSignedDate DATE,
+agreementSignedTime TIME
+);
+
+CREATE TABLE CustomerInfo(
+Id INT AUTO_INCREMENT PRIMARY KEY,
+customerId INT,
+maritalStatus VARCHAR(50),
+children BIT,
+accommodation VARCHAR(50)
+);
+
+ALTER TABLE Agreement ADD FOREIGN KEY (customerId) REFERENCES Customer(Id);
+ALTER TABLE Agreement ADD FOREIGN KEY (productId) REFERENCES Product(Id);
+
+ALTER TABLE CustomerInfo ADD FOREIGN KEY (customerId) REFERENCES Customer(Id);
+
+
+
