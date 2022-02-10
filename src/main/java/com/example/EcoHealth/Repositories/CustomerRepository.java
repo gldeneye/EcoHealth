29b2887b2924be57db42f9c1f97401f81698499b
Robyn Doyle <robyn.doylee@hotmail.com>
2022-02-10 11:29:04 +0100

    robyns commit

--- src/main/java/com/example/EcoHealth/Repositories/CustomerRepository.java
+++ src/main/java/com/example/EcoHealth/Repositories/CustomerRepository.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,16 +37,12 @@ public class CustomerRepository {
     public Boolean checkPassword(String persNo, String password){
         Boolean result = false;
         try (Connection conn = dataSource.getConnection();
-             Statement statement = conn.createStatement();
-             ResultSet rs = statement.executeQuery("SELECT * FROM CUSTOMER WHERE PERSNO = '"+persNo +"'")) {
+             PreparedStatement ps = conn.prepareStatement("SELECT * FROM CUSTOMER WHERE PERSNO = '"+persNo +"'")) {
+                ps.setString(1, persNo);
+                ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                Customer customer = new Customer(rs.getString("persNo"), rs.getString("password"), rs.getString("firstName"), rs.getString("lastName"));
-
-                if (customer.getPassword().equals(password)){
-                    result = true;
-                }
-
+                result = true;
             }
 
         } catch (SQLException e) {
@@ -59,29 +52,4 @@ public class CustomerRepository {
         return result;
     }
 
-    public Customer getCustomerInfo(String persNo){
-
-        //custRepo.getCustomerInfo('931104-0125') -->
-        Customer customer;
-
-        try (Connection conn = dataSource.getConnection();
-             Statement statement = conn.createStatement();
-             ResultSet rs = statement.executeQuery("SELECT * FROM CUSTOMER WHERE PERSNO = '"+persNo +"' INNER JOIN CUSTOMERINFO ON CUSTOMER.Id = CUSTOMERINFO.CUSTOMERID")) {
-
-            if (rs.next()) {
-                customer = new Customer(rs.getString("persNo"), rs.getString("password"), rs.getString("firstName"), rs.getString("lastName"));
-
-                if/while ()){
-
-                }
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return customer;
-
-    }
 }
