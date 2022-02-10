4add64bd872496f390e15908cdc594f8031aa51c
Robyn Doyle <robyn.doylee@hotmail.com>
2022-02-10 08:55:35 +0100

    robyns commit

--- src/main/java/com/example/EcoHealth/Customer.java
+++ src/main/java/com/example/EcoHealth/Customer.java
@@ -8,10 +8,12 @@ public class Customer {
     private String lastName;
     private String email;
 
-    //förenklad constructor för att få DB att fungera.
+//förenklad constructor för att få DB att fungera.
 // tänk på att ändra i repots getcustomers metod i "new customer"
-    //i DB endast en tabell just nu va rtvungen att radera
-    //lägg till unique i customer tabell på persNo
+//i DB endast en tabell just nu va rtvungen att radera
+//lägg till unique i customer tabell på persNo
+    //spelar ingen roll vilka villkor som ligger i databas (unique, not null etc) då constructor styr hur man kan skapar objekt
+
     public Customer(String persNo, String firstName, String lastName) {
         this.persNo = persNo;
         this.firstName = firstName;
