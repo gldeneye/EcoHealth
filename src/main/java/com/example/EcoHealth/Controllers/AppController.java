849eec897b8cd985a2de87c288255d67408e7429
Robyn Doyle <robyn.doylee@hotmail.com>
2022-02-14 16:31:21 +0100

    resultpage: 2 loopar

--- src/main/java/com/example/EcoHealth/Controllers/AppController.java
+++ src/main/java/com/example/EcoHealth/Controllers/AppController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -22,39 +23,39 @@ public class AppController {
     private CustomerRepository customerRepository;
 
     @GetMapping("/")
-    public String homePage () {
+    public String homePage() {
         return "login";
     }
 
     @PostMapping("/check")
     public String kontrolleraInlog(HttpSession session, Model model, @RequestParam String persNo, @RequestParam String password) {
-       boolean check = customerRepository.checkPassword(persNo, password);
-       if (check) {
-           Customer customer = new Customer();
-           customer.setPersNo(persNo);
-           model.addAttribute("customer",customer);
+        boolean check = customerRepository.checkPassword(persNo, password);
+        if (check) {
+            Customer customer = new Customer();
+            customer.setPersNo(persNo);
+            model.addAttribute("customer", customer);
 
-           boolean hasMortgage = customerRepository.checkProduct(persNo,"Mortgage");
-           boolean hasBufferSavings = customerRepository.checkProduct(persNo,"BufferSavings");
-           boolean hasChildrensSavings = customerRepository.checkProduct(persNo,"ChildrensSavings");
-           boolean hasPensionsSavings = customerRepository.checkProduct(persNo,"PensionsSavings");
-           boolean hasInsurance = customerRepository.checkProduct(persNo,"Insurance");
+            boolean hasMortgage = customerRepository.checkProduct(persNo, "Mortgage");
+            boolean hasBufferSavings = customerRepository.checkProduct(persNo, "BufferSavings");
+            boolean hasChildrensSavings = customerRepository.checkProduct(persNo, "ChildrensSavings");
+            boolean hasPensionsSavings = customerRepository.checkProduct(persNo, "PensionsSavings");
+            boolean hasInsurance = customerRepository.checkProduct(persNo, "Insurance");
 
-           boolean hasChildren = customerRepository.getHasChildren(persNo);
-           customer.setHasChildren(hasChildren);
+            boolean hasChildren = customerRepository.getHasChildren(persNo);
+            customer.setHasChildren(hasChildren);
 
-           String maritalStatus = customerRepository.getMaritalOrAccommodationStatus(persNo, "maritalStatus");
-           customer.setMaritalStatus(maritalStatus);
+            String maritalStatus = customerRepository.getMaritalOrAccommodationStatus(persNo, "maritalStatus");
+            customer.setMaritalStatus(maritalStatus);
 
-           String typeOfLiving = customerRepository.getMaritalOrAccommodationStatus(persNo, "accommodation");
-           customer.setTypeOfLiving(typeOfLiving);
+            String typeOfLiving = customerRepository.getMaritalOrAccommodationStatus(persNo, "accommodation");
+            customer.setTypeOfLiving(typeOfLiving);
 
-           String getEmail = customerRepository.getEmail(persNo);
-           customer.setEmail(getEmail);
+            String getEmail = customerRepository.getEmail(persNo);
+            customer.setEmail(getEmail);
 
-           String retrieveFullName = customerRepository.getFullName(persNo);
-           customer.setFirstName(retrieveFullName.split("_")[0]);
-           customer.setLastName(retrieveFullName.split("_")[1]);
+            String retrieveFullName = customerRepository.getFullName(persNo);
+            customer.setFirstName(retrieveFullName.split("_")[0]);
+            customer.setLastName(retrieveFullName.split("_")[1]);
 
 //           System.out.println(customer.getFirstName() + "" + customer.getLastName());
 //           System.out.println("Has children: " + hasChildren);
@@ -63,11 +64,11 @@ public class AppController {
 //           System.out.println("Email: " + getEmail);
 
 
-           model.addAttribute("hasMortgage", hasMortgage);
-           model.addAttribute("hasBufferSavings", hasBufferSavings);
-           model.addAttribute("hasChildSavings", hasChildrensSavings);
-           model.addAttribute("hasInsurance", hasInsurance);
-           model.addAttribute("hasPensionSavings", hasPensionsSavings);
+            model.addAttribute("hasMortgage", hasMortgage);
+            model.addAttribute("hasBufferSavings", hasBufferSavings);
+            model.addAttribute("hasChildSavings", hasChildrensSavings);
+            model.addAttribute("hasInsurance", hasInsurance);
+            model.addAttribute("hasPensionSavings", hasPensionsSavings);
 
 //           System.out.println("Mortgage: " + hasMortgage);
 //           System.out.println("Buffer: " + hasBufferSavings);
@@ -75,21 +76,20 @@ public class AppController {
 //           System.out.println("Insurance: " + hasInsurance);
 //           System.out.println("Pension Savings: " + hasPensionsSavings);
 
-           session.setAttribute("hasMortgage", hasMortgage);
-           session.setAttribute("hasBufferSavings", hasBufferSavings);
-           session.setAttribute("hasChildSavings", hasChildrensSavings);
-           session.setAttribute("hasInsurance", hasInsurance);
-           session.setAttribute("hasPensionSavings", hasPensionsSavings);
+            session.setAttribute("hasMortgage", hasMortgage);
+            session.setAttribute("hasBufferSavings", hasBufferSavings);
+            session.setAttribute("hasChildSavings", hasChildrensSavings);
+            session.setAttribute("hasInsurance", hasInsurance);
+            session.setAttribute("hasPensionSavings", hasPensionsSavings);
 
-           return "form";
-       }
-       else {
-           return "redirect:/";
-       }
+            return "form";
+        } else {
+            return "redirect:/";
+        }
     }
 
     @PostMapping("/form")
-    public String saveInfo (HttpSession session, Model model, @ModelAttribute Customer customer) {
+    public String saveInfo(HttpSession session, Model model, @ModelAttribute Customer customer) {
         int tokens = customerRepository.calcCustomerTokens(customer.getPersNo());
         System.out.println(tokens);
         model.addAttribute("tokens", tokens);
@@ -97,16 +97,79 @@ public class AppController {
         System.out.println("Kund har bolån? " + session.getAttribute("hasMortgage"));
 
 
-        List<Boolean> agreements = new ArrayList<>();
-        agreements.add((boolean) session.getAttribute("hasMortgage"));
-        agreements.add((boolean) session.getAttribute("hasBufferSavings"));
-        agreements.add((boolean) session.getAttribute("hasChildSavings"));
-        agreements.add((boolean) session.getAttribute("hasInsurance"));
-        agreements.add((boolean) session.getAttribute("hasPensionSavings"));
-        model.addAttribute("agreements", agreements);
+        HashMap<String, String> hasAgreements = new HashMap<>();
+        HashMap<String, String> doesNotHaveAgreements = new HashMap<>();
 
-        System.out.println(agreements.get(0));
+        if ((boolean) session.getAttribute("hasMortgage")) {
+            hasAgreements.put("hasMortgage", "Bolån");
+        } else {
+            doesNotHaveAgreements.put("hasMortgage", "Bolån");
+        }
+        ;
 
+        if ((boolean) session.getAttribute("hasChildSavings")) {
+            hasAgreements.put("hasChildSavings", "Sparande till barn");
+        } else {
+            doesNotHaveAgreements.put("hasChildSavings", "Sparande till barn");
+        }
+        ;
+
+        if ((boolean) session.getAttribute("hasBufferSavings")) {
+            hasAgreements.put("hasBufferSavings", "Buffertsparande");
+        } else {
+            doesNotHaveAgreements.put("hasBufferSavings", "Buffertsparande");
+        }
+        ;
+
+        if ((boolean) session.getAttribute("hasInsurance")) {
+            hasAgreements.put("hasInsurance", "Försäkring/-ar");
+        } else {
+            doesNotHaveAgreements.put("hasInsurance", "Försäkring/-ar");
+        }
+        ;
+
+        if ((boolean) session.getAttribute("hasPensionSavings")) {
+            hasAgreements.put("hasPensionSavings", "Pensionssparande");
+        } else {
+            doesNotHaveAgreements.put("hasPensionSavings", "Pensionssparande");
+        };
+
+        /*agreements.put("buffert", (boolean) session.getAttribute("hasBufferSavings"));
+        agreements.put("hasChildSavings", (boolean) session.getAttribute("hasChildSavings"));
+        agreements.put("hasInsurance", (boolean) session.getAttribute("hasInsurance"));
+        agreements.put("hasPensionSavings", (boolean) session.getAttribute("hasPensionSavings"));*/
+
+        model.addAttribute("hasAgreements", hasAgreements);
+        model.addAttribute("doesNotHaveAgreements", doesNotHaveAgreements);
+
+        boolean show = false;
+        if (hasAgreements.size()>0) {
+            show = true;
+        }
+
+        boolean show2 = false;
+        if (doesNotHaveAgreements.size()>0) {
+            show2 = true;
+        }
+
+        model.addAttribute("show2", show2);
+        model.addAttribute("show", show);
+
+/*
+        Product Mortgage = new Product("Mortgage", "Flytta ditt bolån till Handelsbanken och tjäna XX st Scotte coins","#");
+        Product Buffer = new Product("Buffer", "Trygga din ekonomi med en buffert för oförusedda utgifter", "#");
+        Product ChildrenSavings = new Product("ChildrenSavings","Barnspar", "#");
+        Product PensionSavings = new Product("PensionSavings", "Pensionsspar", "#");
+        Product Insurance = new Product("Insurance","Försäkring", "#");
+
+        HashMap<Integer, Product> products = new HashMap<>();
+        products.put(1, Mortgage);
+        products.put(2, Buffer);
+        products.put(3, ChildrenSavings);
+        products.put(4, PensionSavings);
+        products.put(5, Insurance);
+
+        model.addAttribute("products", products);*/
 
         return "result";
     }
---
+++ src/main/java/com/example/EcoHealth/Controllers/Product.java
@@ -0,0 +1,37 @@
+package com.example.EcoHealth.Controllers;
+
+public class Product {
+    private String name;
+    private String description;
+    private String href;
+
+    public Product(String name, String description, String href) {
+        this.name = name;
+        this.description = description;
+        this.href = href;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+}
